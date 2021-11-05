7d39d45a27fbadd9bd2c069a810f864ed68030a9
Ken Irwin <ken@kenirwin.net>
2021-11-05 17:16:42 -0400

    basic node/express app

---
+++ app.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const MemoryStore = require('session-memory-store')(session);
+
+const app = express();
+app.use(cookieParser());
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  res.send('Hello world!');
+});
+
+const PORT = '9909';
+app.listen(PORT, function () {
+  console.log(
+    `Localhost app listening on port ${PORT}! Go to http://localhost:${PORT}/`
+  );
+});
