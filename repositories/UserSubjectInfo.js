8c2c09c671f28dc3f84951348864d3005a21d94c
Ken Irwin <ken@kenirwin.net>
2021-11-16 13:39:27 -0500

    UserSubjectInfo adds addSubjectsFromMajors,  addSubjectsFromCourses

--- repositories/UserSubjectInfo.js
+++ repositories/UserSubjectInfo.js
@@ -3,12 +3,20 @@ const _ = require('lodash');
 module.exports = class UserSubjectInfo {
   constructor(userLoginInfo, subjectMap) {
     this.user = userLoginInfo;
+    this.courses = [];
     this.map = subjectMap;
     this.subjects = [];
     this.subjectNames = [];
     this.libguideSubjects = [];
+    if (this.user.attr.hasOwnProperty('coursesTaken')) {
+      this.courses = this.courses.concat(this.user.attr.coursesTaken);
+    }
+    if (this.user.attr.hasOwnProperty('coursesTaught')) {
+      this.courses = this.courses.concat(this.user.attr.coursesTaught);
+    }
   }
 
+  /* RegCode methods */
   getSubjByRegCode(regCode) {
     let res = _.filter(this.map, { regCodes: [{ regCode: regCode }] });
     if (res.length == 1) {
@@ -25,5 +33,35 @@ module.exports = class UserSubjectInfo {
       return found[1];
     }
     return false;
+  }
+
+  addSubjectsFromCourses() {
+    this.courses.forEach((courseNumber) => {
+      let regCode = this.getRegCodeFromCourseNumber(courseNumber);
+      let subject = this.getSubjByRegCode(regCode);
+      if (subject && !this.subjects.includes(subject)) {
+        this.subjects.push(subject);
+      }
+    });
+  }
+
+  /* MajorCode methods */
+
+  getSubjByMajorCode(majorCode) {
+    let res = _.filter(this.map, { majorCodes: [{ majorCode: majorCode }] });
+    if (res.length == 1) {
+      return res[0];
+    }
+    return false;
+  }
+
+  addSubjectsFromMajors() {
+    let majorCodes = this.user.attr.majorCodes;
+    majorCodes.forEach((majorCode) => {
+      let subject = this.getSubjByMajorCode(majorCode);
+      if (subject && !this.subjects.includes(subject)) {
+        this.subjects.push(subject);
+      }
+    });
   }
 };
--- test/repositories/UserSubjectInfoTest.js
+++ test/repositories/UserSubjectInfoTest.js
@@ -40,30 +40,37 @@ describe('UserSubjectInfo: constructor', () => {
     expect(obj.libguideSubjects).to.be.an.array();
     expect(obj.libguideSubjects.length).to.equal(0);
   });
+  it('should have a property "courses" based on user.attr.coursesTaken', () => {
+    expect(obj).to.have.property('courses');
+    expect(obj.courses).to.be.an.array();
+    expect(obj.courses.length).to.equal(7);
+    expect(obj.courses[0]).to.equal('SPN101');
+  });
 });
 
+/* testing regCode methods */
 describe('UserSubjectInfo: getSubjByRegCode', () => {
   let res = obj.getSubjByRegCode('ACC');
   let res2 = obj.getSubjByRegCode('AA');
   let badRes = badObj.getSubjByRegCode('ACC');
 
-  it('should return a single object for regCode with name "Accountancy" for "ACC"', () => {
+  it('should return a single object with name "Accountancy" for "ACC"', () => {
     expect(res).to.be.an('object');
     expect(res).to.have.property('name');
     expect(res.name).to.equal('Accountancy');
   });
-  it('should return a single object for regCode with libguides ["Accountancy"] for "ACC"', () => {
+  it('should return a single object with libguides ["Accountancy"] for "ACC"', () => {
     expect(res).to.be.an('object');
     expect(res).to.have.property('libguides');
     expect(res.libguides.length).to.equal(1);
     expect(res.libguides).to.be.equalTo(['Accountancy']);
   });
-  it('should return a single object for regCode with name "Academic Affairs" for "ACC"', () => {
+  it('should return a single object with name "Academic Affairs" for "AA"', () => {
     expect(res2).to.be.an('object');
     expect(res2).to.have.property('name');
     expect(res2.name).to.equal('Academic Affairs');
   });
-  it('should return a single object for regCode with libguides ["Accountancy"] for "ACC"', () => {
+  it('should return a single object with libguides ["Educational Leadership"] for "AA"', () => {
     expect(res2).to.be.an('object');
     expect(res2).to.have.property('libguides');
     expect(res2.libguides.length).to.equal(1);
@@ -106,5 +113,39 @@ describe('UserSubjectInfo: getRegCodeFromCourseNumber', () => {
   it('should get false from "143sdalKA" with no pattern specified', () => {
     let res = obj.getRegCodeFromCourseNumber('143sdalKA');
     expect(res).to.be.false;
+  });
+});
+
+describe('UserSubjectInfo: addSubjectsFromCourses', () => {
+  // this should get some stub tests
+});
+
+/* testing regCode methods */
+
+describe('UserSubjectInfo: getSubjByRegCode', () => {
+  let res = obj.getSubjByMajorCode('BU01');
+  let res2 = obj.getSubjByMajorCode('EA18');
+
+  it('should return a single object with name "Accountancy" for "BU01"', () => {
+    expect(res).to.be.an('object');
+    expect(res).to.have.property('name');
+    expect(res.name).to.equal('Accountancy');
+  });
+  it('should return a single object with libguides ["Accountancy"] for "BU01"', () => {
+    expect(res).to.be.an('object');
+    expect(res).to.have.property('libguides');
+    expect(res.libguides.length).to.equal(1);
+    expect(res.libguides).to.be.equalTo(['Accountancy']);
+  });
+  it('should return a single object with name "Adolescent Education" for "EA18"', () => {
+    expect(res2).to.be.an('object');
+    expect(res2).to.have.property('name');
+    expect(res2.name).to.equal('Adolescent Education');
+  });
+  it('should return a single object with libguides ["Education"] for "EA18"', () => {
+    expect(res2).to.be.an('object');
+    expect(res2).to.have.property('libguides');
+    expect(res2.libguides.length).to.equal(1);
+    expect(res2.libguides).to.be.equalTo(['Education']);
   });
 });
