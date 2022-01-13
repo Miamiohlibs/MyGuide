0fcb618c7922347a14994c53e0b18ff0038b51be
Ken Irwin <ken@kenirwin.net>
2022-01-13 13:59:30 -0500

    added circData to / and /json routes

--- routes.js
+++ routes.js
@@ -1,13 +1,17 @@
 const config = require('config');
 const getUserLoginInfo = require('./services/getUserInfo');
+const CircController = require('./controllers/CirculationController');
+const circController = new CircController();
 const fs = require('fs');
 module.exports = function (app) {
   app.get('/', async (req, res) => {
     let user = getUserLoginInfo(req);
+    let circData = await circController.getUserData(user.person.userId);
     // res.send(userInfo);
     // res.render('dashboard', { user: userInfo, settings: settings });
     res.render('index', {
       user: user,
+      circ: circData,
       fs: fs,
       config: config.get('viewConfigs'),
     });
@@ -15,6 +19,7 @@ module.exports = function (app) {
 
   app.get('/json', async (req, res) => {
     let userInfo = getUserLoginInfo(req);
-    res.send(userInfo);
+    let circData = await circController.getUserData(userInfo.person.userId);
+    res.send({ circ: circData, user: userInfo });
   });
 };
