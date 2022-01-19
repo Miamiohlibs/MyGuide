ba7c39855f96e206e9b1b14bd463e547341f349c
Ken Irwin <ken@kenirwin.net>
2022-01-19 12:20:44 -0500

    get all possible filenames for a subject

--- utilities/updateSubjectCache.js
+++ utilities/updateSubjectCache.js
@@ -16,8 +16,42 @@ const librarians = require(rootdir + '/cache/Librarians');
 const databases = require(rootdir + '/cache/Databases');
 const guides = require(rootdir + '/cache/Guides');
 const subjects = require(rootdir + '/cache/Subjects');
+const filenamify = require(rootdir + '/utilities/filenamify');
 const subjectConfigFilename = config.get('app.subjectConfigFilename');
 const subjectConfigPath = path.join(rootdir, 'config', subjectConfigFilename);
 console.log('subjectConfigPath: ', subjectConfigPath);
 // read the subject config file
 const subjectConfig = JSON.parse(fs.readFileSync(subjectConfigPath, 'utf8'));
+
+const allowedGroups = config.get('LibGuides.allowedGroupIds');
+const LibAppsDataFilter = require(rootdir + '/repositories/LibAppsDataFilter');
+const f = new LibAppsDataFilter();
+
+subjectConfig.forEach((subject) => {
+  let subjects = subject.libguides;
+  let subjectNames = getAllSubjectNames(subject);
+  console.log('subjectNames: ', subjectNames);
+});
+
+function getAllSubjectNames(entry) {
+  let allSubjectNames = [entry.name];
+  let codeTypes = ['major', 'reg', 'dept'];
+  codeTypes.forEach((codeType) => {
+    if (entry.hasOwnProperty(codeType + 'Codes')) {
+      let codes = entry[codeType + 'Codes'];
+      let subjectNames = codes.map((code) => {
+        return code[codeType + 'Name'];
+      });
+      // merge subjectNames into allCodes
+      allSubjectNames = allSubjectNames.concat(subjectNames);
+    }
+  });
+  // remove undefined values
+  allSubjectNames = allSubjectNames.filter((name) => {
+    return name !== undefined;
+  });
+  allSubjectNames = allSubjectNames.map((str) => filenamify(str));
+  //eliminate duplicates
+  allSubjectNames = [...new Set(allSubjectNames)];
+  return allSubjectNames;
+}
