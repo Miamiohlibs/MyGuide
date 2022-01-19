3e9d4f1dc33c101c491e11da86c797920a2dd453
Ken Irwin <ken@kenirwin.net>
2022-01-19 09:52:15 -0500

    read the subject config file

---
+++ utilities/updateSubjectCache.js
@@ -0,0 +1,23 @@
+/*
+    foreach item in miami_subjects
+    identify the libguides subject -> subjects
+    from each subject get the libns, guides, and databases + metadata
+
+    foreach name, majorName, deptName, regName: write out a file with all that info
+*/
+
+const fs = require('fs');
+const path = require('path');
+const rootdir = path.dirname(__dirname);
+const confPath = path.join(rootdir, 'config');
+process.env['NODE_CONFIG_DIR'] = confPath;
+const config = require('config');
+const librarians = require(rootdir + '/cache/Librarians');
+const databases = require(rootdir + '/cache/Databases');
+const guides = require(rootdir + '/cache/Guides');
+const subjects = require(rootdir + '/cache/Subjects');
+const subjectConfigFilename = config.get('app.subjectConfigFilename');
+const subjectConfigPath = path.join(rootdir, 'config', subjectConfigFilename);
+console.log('subjectConfigPath: ', subjectConfigPath);
+// read the subject config file
+const subjectConfig = JSON.parse(fs.readFileSync(subjectConfigPath, 'utf8'));
