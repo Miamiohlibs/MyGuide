3f7f7540aabbd0d7e6a5b0c14bb6efaec7de5098
Ken Irwin <ken@kenirwin.net>
2022-02-07 09:48:49 -0500

    log user hits in logs/usageLog.txt

---
+++ models/usageLog/logUser.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs-extra');
+const dayjs = require('dayjs');
+const hash = require('sha256');
+const outputPath = path.join(__dirname, '..', '..', 'logs', 'usageLog.txt');
+fs.ensureFileSync(outputPath);
+
+const logUser = function (u) {
+  let now = dayjs().format();
+  let data = {
+    time: now,
+  };
+  if (u.person.userId) {
+    data.user = hash(u.person.userId);
+  }
+  if (u.courseDepts) {
+    data.courseDepts = u.courseDepts;
+  }
+  if (u.person.majorNames) {
+    data.majors = u.person.majorNames;
+  }
+  if (u.person.departmentName) {
+    data.departments = u.person.departmentName;
+  }
+  if (u.person.divisionName) {
+    data.divisions = u.person.divisionName;
+  }
+  if (u.uniqueSubjects) {
+    data.subjects = u.uniqueSubjects;
+  }
+  if (u.person.userType) {
+    data.primaryAffiliation = u.person.userType;
+  }
+  fs.appendFile(outputPath, '\n' + JSON.stringify(data));
+};
+
+module.exports = logUser;
