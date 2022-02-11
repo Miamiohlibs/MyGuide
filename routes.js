a5fbac2bc68fa64a9430e01fce6f1c3bd3fbc762
Ken Irwin <ken@kenirwin.net>
2022-02-11 15:55:35 -0500

    supports favorite guides & dbs

--- routes.js
+++ routes.js
@@ -115,14 +115,33 @@ module.exports = function (app) {
     const userDataController = new UserDataController(req);
     let user = await userDataController.getUserData();
     userId = user.person.userId;
-    console.log('adding favorite guide ' + req.body.guideId);
+    console.log('adding favorite guide ' + req.body.resourceId);
     let addResponse = await userFavoritesController.updateFavoriteAdd(
       userId,
       'guide',
-      req.body.guideId
+      req.body.resourceId
     );
     if (addResponse.success) {
       res.writeHead(201);
+      res.end();
+    } else {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+  app.post('/favorites/guides/remove', async (req, res) => {
+    console.log('request to route: favorites/guides/remove');
+    const userDataController = new UserDataController(req);
+    let user = await userDataController.getUserData();
+    userId = user.person.userId;
+    console.log('adding favorite guide ' + req.body.resourceId);
+    let removeResponse = await userFavoritesController.updateFavoriteRemove(
+      userId,
+      'guide',
+      req.body.resourceId
+    );
+    if (removeResponse.success) {
+      res.writeHead(202);
       res.end();
     } else {
       res.writeHead(500);
