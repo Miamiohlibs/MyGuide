f20c7f7f519517a8eadac8b7c0452fa4619a91fd
Ken Irwin <ken@kenirwin.net>
2022-07-15 15:08:55 -0400

    tidied stats router

--- routes/stats.js
+++ routes/stats.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const getUsageData = require('../helpers/getUsageData');
 const router = require('express').Router();
 
-/* Graphing and stats Routes */
+/* Graphing Routes */
 
 router.get('/', (req, res) => {
   res.redirect('/stats/usage');
@@ -14,6 +14,11 @@ router.get('/repeatUsers', (req, res) => {
   // res.send('Test');
   res.render('stats-graphRepeats', { page: 'repeatUsers' });
 });
+router.get('/subjects', (req, res) => {
+  res.render('stats-graphSubjects', { page: 'subjectGraph' });
+});
+
+/* JSON stats routes */
 
 router.get('/usageData', (req, res) => {
   // set default params
@@ -30,49 +35,24 @@ router.get('/usageData', (req, res) => {
   res.end(JSON.stringify(stats));
 });
 
+router.get('/repeatData', async (req, res) => {
+  data = getUsageData();
+  let repeatUsers = require('../helpers/getRepeatUsers');
+  let repeatData = repeatUsers(data, req.query);
+  res.setHeader('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      repeatData,
+    })
+  );
+});
+
 router.get('/subjectData', (req, res) => {
   data = getUsageData();
   let getSubjectStats = require('../helpers/getSubjectStats');
   let stats = getSubjectStats(data, req.query);
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ stats }));
-});
-
-router.get('/subjects', (req, res) => {
-  res.render('stats-graphSubjects', { page: 'subjectGraph' });
-});
-
-router.get('/repeatData', async (req, res) => {
-  data = getUsageData();
-  let repeatUsers = require('../helpers/getRepeatUsers');
-  let repeatData = repeatUsers(data, req.query);
-
-  // let allSummary = repeatUsers(data, {
-  //   startDate: '2021-09-02',
-  //   breakpoint: 10,
-  // });
-  // let facSummary = repeatUsers(data, {
-  //   population: 'faculty',
-  //   startDate: '2021-09-02',
-  // });
-  // let staffSummary = repeatUsers(data, {
-  //   population: 'staff',
-  //   startDate: '2021-09-02',
-  // });
-  // let stuSummary = repeatUsers(data, {
-  //   population: 'student',
-  //   startDate: '2021-09-02',
-  // });
-  res.setHeader('Content-Type', 'application/json');
-  res.end(
-    JSON.stringify({
-      repeatData,
-      // student: stuSummary,
-      // faculty: facSummary,
-      // staff: staffSummary,
-      // all: allSummary,
-    })
-  );
 });
 
 module.exports = router;
