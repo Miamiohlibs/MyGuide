2b7ffa20c05c809000109acbe77673cfaa38254c
Ken Irwin <ken@kenirwin.net>
2022-08-18 10:57:05 -0400

    nonce tests in temp.js

--- temp.js
+++ temp.js
@@ -1,7 +1,34 @@
-const getUsageData = require('./helpers/getUsageData');
-// const reportUsage = require('./helpers/reportUsage');
-const getRepeatUsers = require('./helpers/getRepeatUsers');
-let data = getUsageData();
+const express = require('express');
+const helmet = require('helmet');
 
-let report = getRepeatUsers(data, {});
-console.log(report);
+// const directives = require('./helpers/contentSecurityPolicy');
+
+// console.log(directives);
+
+let nonce = '1234567890';
+
+const app = express();
+app.use(helmet.frameguard());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'nonce-" + nonce + "'",
+        "'sha256-tF61fkF9jqKAYfyKLyk2kPTFFkV99XI3doRxVepVXU8='",
+      ],
+    },
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send(
+    '<script nonce=' +
+      nonce +
+      '>console.log("Nonce in action")</script><body>Plain HTML</body>' +
+      '<script>console.log("Hash in Action");</script>'
+  );
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
