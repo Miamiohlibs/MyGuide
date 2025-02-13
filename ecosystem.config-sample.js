fbd84abd5f525b9a025f86951350910a450b078b
Ken Irwin <ken@kenirwin.net>
2025-02-13 15:01:06 -0500

    revised pm2 ecosystem directions

--- ecosystem.config-sample.js
+++ ecosystem.config-sample.js
@@ -1,31 +1,30 @@
 /* 
     This is a sample configuration file for PM2. It is used to manage the MyGuide app and 
-    its associated data retrieval script. The app is run in both production and development 
-    environments. The data retrieval script is run once a week to update the app's data 
-    cache. The app is restarted after the data retrieval script runs to ensure the cache is 
-    up to date.
+    its associated data retrieval script. The data retrieval script is run once a week to update 
+    the app's data cache. The app is restarted after the data retrieval script runs to ensure the 
+    cache is up to date.
 
     This file should be saved as ecosystem.config.js in the root directory of the app. If
     you are running multiple instances of the app (such as a production and development
-    versions shown in this sample), you only need one ecosystem.config.js file to run both 
-    instances. 
+    versions shown in this sample), each instance should have its own ecosystem.config.js file.
+
+    Clear the existing instances before setting up the ecosystem:
+    `pm2 delete all`
 
     Instantiate the app with the following command:
-    pm2 start ecosystem.config.js
+    `pm2 start /path/to/ecosystem.config.js`
 
-    To save the configuration file, run the following command:
-    pm2 save
+    If running more than one instance, also instantiate the other instance with:
+    `pm2 start /path/to/other/ecosystem.config.js`
 
-    Finally, to ensure that the app and data retrieval script restart automatically after a 
-    server reboot, run the following command:
-    pm2 startup
+    To save the configurations for one or more, run the following command:
+    `pm2 save`
 */
 
 module.exports = {
   apps: [
-    // MyGuide app -- production
     {
-      name: 'myguide-prod',
+      name: 'myguide-prod', // change this as you wish
       script: '/path/to/myguide/MyGuide/app.js',
       env: {
         NODE_ENV: 'production',
@@ -40,27 +39,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
       },
-      post_update: ['pm2 restart myguide-prod'],
-    },
-
-    // MyGuide app -- development
-    {
-      name: 'myguide-dev',
-      script: '/path/to/myguidedev/MyGuide/app.js',
-      env: {
-        NODE_ENV: 'development',
-      },
-    },
-    {
-      name: 'myguide-getdata-dev',
-      script: '/path/to/myguidedev/MyGuide/getData.sh',
-      autorestart: false,
-      cron_restart: '0 3 * * 6', // Runs every Saturday at 3:00 AM
-      exec_interpreter: 'bash',
-      env: {
-        NODE_ENV: 'development',
-      },
-      post_update: ['pm2 restart myguide-dev'],
+      post_update: ['pm2 restart myguide-prod'], // the last argument should match the app named in apps[0]
     },
   ],
 };
